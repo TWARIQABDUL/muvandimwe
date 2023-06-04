334d26dd1b012ce54829a13b1668a6072f3f2a0e
TWARIQABDUL <tariqazizibabu@gmail.com>
2023-06-04 16:07:11 +0200

    dynamic drivers list implemented

--- src/components/driver.jsx
+++ src/components/driver.jsx
@@ -1,30 +1,62 @@
-import React, {useContext} from 'react'
+import React, { useEffect, useState } from 'react';
 import LetterAvatar from 'react-letter-avatar';
-import { SessionContext } from '../session';
-import Loading from './loadinf';
-function Driver() {
-    const { session } = useContext(SessionContext);
-    function extractLetter(string) {
-        return string.charAt(0).toUpperCase();
-    }
-    if (session == null) {
-        return <Loading/>
-    }else{
-        const letel = extractLetter(session.displayName)
-        return (
-            <div className='driver-holder'>
-                <div className="driver-profile">
-                    <LetterAvatar name={letel} size={50} radius={50} />
-                    {session.displayName}
-                </div>
-                <div className="driver-info">
-                    <p>0.5Km away</p>
-                    <p>5/7 rating</p>
-                    <p>BMWz 8 seats</p>
-                </div>
-            </div>
-        )
+import { Link } from 'react-router-dom';
+import L from 'leaflet';
+import { Geolocation } from '@capacitor/geolocation';
+
+function Driver({ lists }) {
+  const [position, setPosition] = useState(null);
+  const [distance, setDistance] = useState(null);
+
+  useEffect(() => {
+    const checkLocationPermission = async () => {
+      try {
+        const permissionStatus = await Geolocation.checkPermissions();
+        const hasPermission = permissionStatus.location === 'granted';
+        if (hasPermission) {
+          const { coords } = await Geolocation.getCurrentPosition();
+          setPosition({ lat: coords.latitude, lng: coords.longitude });
         }
+      } catch (error) {
+        setPosition(null);
+      }
+    };
+
+    checkLocationPermission();
+  }, []);
+
+  useEffect(() => {
+    if (position) {
+      const myCurrentPossition = [position.lat, position.lng];
+      const driverLocation = [lists.location._lat, lists.location._long];
+      const myPosition = L.latLng(myCurrentPossition[0], myCurrentPossition[1]);
+      const driverPosition = L.latLng(driverLocation[0], driverLocation[1]);
+      const distanceInMeters = myPosition.distanceTo(driverPosition);
+      const distanceInKilometers = distanceInMeters / 1000;
+      setDistance(distanceInKilometers.toFixed(2));
+    }
+  }, [position]);
+
+  function extractLetter(string) {
+    return string.charAt(0).toUpperCase();
+  }
+// console.log(lists);
+  const letel = extractLetter(lists.name);
+  return (
+    <div className="driver-holder">
+      <Link to="jack" className="link-black">
+        <div className="driver-profile">
+          <LetterAvatar name={letel} size={50} radius={50} />
+          {lists.name}
+        </div>
+        <div className="driver-info">
+          {distance ? <p>{distance} Km away</p> : <p>Loading distance...</p>}
+          <p>{lists.ratings / lists.counts} rating</p>
+          <p>{lists.type }  {lists.seats} seats</p>
+        </div>
+      </Link>
+    </div>
+  );
 }
 
-export default Driver
+export default Driver;
--- src/components/rideList.jsx
+++ src/components/rideList.jsx
@@ -1,12 +1,36 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Driver from './driver'
+import { collection, getDocs } from '@firebase/firestore'
+import { db } from '../firebase'
+import Loading from './loadinf'
 
 function DriverList() {
-
+const [loaded,setLoaded] = useState(false)
+const [driverList,setDriverList] = useState([])
+useEffect(()=>{
+  const fetchDrivers = async ()=>{
+    try{
+      const querySnapshot = await getDocs(collection(db,"drivers"))
+      const items = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data()}));
+      setDriverList(items)
+      setLoaded(true)
+    }catch{
+      console.log("some error acured");
+    }
+  }
+  fetchDrivers()
+})
+if (loaded) {
+  const Drivers = driverList.map((drive)=> <Driver key={drive.id} lists={drive}/>)
+  return(
+    <div className='drivers-list-holde'>
+      {Drivers}
+    </div>
+  )
+}
   return (
     <div className='drivers-list-holde'>
-      <Driver />
-      <Driver />
+      <Loading/>
     </div>
   )
 }
