76f049aa040bf5936e3b878ce103e10e624f5825
TWARIQABDUL <tariqazizibabu@gmail.com>
2023-06-25 13:10:16 +0200

    fixed popup

--- src/components/destination.jsx
+++ src/components/destination.jsx
@@ -6,21 +6,32 @@ import { SessionContext } from '../session';
 import { addDoc, collection } from '@firebase/firestore';
 import { db } from '../firebase';
 import { useLocation } from 'react-router-dom';
+import { Dialog } from '@capacitor/dialog';
 function Destination() {
-    const [mes,setMes] = useState()
+    const [mes, setMes] = useState()
 
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
     const id = searchParams.get('driver');
+    const [procced, setProcced] = useState(false)
     const { session } = useContext(SessionContext);
     const [placeName, setPlaceName] = useState('');
     const [destin, setDestination] = useState('');
     const [spot, setSpot] = useState()
     const API_KEY = "5d9c0866e28d49438f54ac4db78c33fe";
-    const getInput = (e) => {
+    const getInput = (e) => { //get the inputs 
         setDestination(e.target.value)
-    }
-    const makeOrder = async (loc) => {
+    }//end 
+
+    const showConfirm = async () => {
+        const { value } = await Dialog.confirm({
+            title: 'Confirm',
+            message: `Are you sure you'd like to order `,
+        });
+        setProcced(value)
+        return value
+    };
+    const makeOrder = async (loc) => { //makes the order
         const object = {
             driverid: id,
             customerid: session.uid,
@@ -37,19 +48,28 @@ function Destination() {
                     console.log(er);
                 })
         } catch (error) {
-            console.log("fuck",error);
+            console.log("fuck", error);
         }
         return mes
-    }
-    const hundleOrder = async () => {
+    }//end order
+    const hundleOrder = async () => {//hundle the order
+
         try {
-            const resultsName = await axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${destin}&key=${API_KEY}`)
-            const { results } = resultsName.data;
-            setSpot(results[0].geometry)
-            makeOrder(results[0].geometry)
-                .then(suc => {
-                    console.log("succes",suc);
-                })
+            showConfirm().then(pr => {
+                if (pr) {
+                    axios.get(`https://api.opencagedata.com/geocode/v1/json?q=${destin}&key=${API_KEY}`)
+                        .then(datas => {
+                            const { results } = datas.data;
+                            setSpot(results[0].geometry)
+                            // console.log(results[0].geometry);
+                            makeOrder(results[0].geometry)
+                                .then(suc => {
+                                    console.log("succes", suc);
+                                })
+                        })
+
+                }
+            })
         } catch (error) {
 
         }
